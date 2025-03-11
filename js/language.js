15de5269145aafd243c690e4921bbd6432965881
Ccanchillo <brunomixan@gmail.com>
2025-03-10 21:44:20 -0500

    Se agrega el Proyecto

---
+++ js/language.js
@@ -0,0 +1,19 @@
+const langButtons = document.querySelectorAll("[data-language]");
+const textsToChange = document.querySelectorAll("[data-section]");
+
+langButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+
+        fetch(`../languages/${button.dataset.language}.json`)
+        .then(res => res.json())
+        .then(data => {
+            textsToChange.forEach((el) => {
+                const section = el.dataset.section;
+                const value = el.dataset.value;
+
+                el.innerHTML = data[section][value];
+            });
+        })
+        .catch(error => console.error("Error al cargar el JSON:", error));
+    });
+});
